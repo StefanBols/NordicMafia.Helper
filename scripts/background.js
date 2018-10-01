2c14b075f56f7fd07dd92467554825f435d1bba5
Stefan Bols <stefan@imbols.net>
2018-10-01 21:09:13 +0200

    Added notification. Need to implement an alarm to set future notifications

--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,27 @@
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    console.log("Received %o from %o, frame", msg, sender.tab, sender.frameId);
-    sendResponse("Gotcha!");
+    // var notification = new Notification('Notification title', {
+    //     icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+    //     body: "Hey there! You've been notified!",
+    //   });
+    console.log('BackgroundScript recieved message: ', msg);
+    sendResponse('Recieved a message from ContentScript');
+
+    if (msg.action && msg.action === 'sendNotification' && msg.payload) {
+        sendNotificationByPayload(msg.payload);
+    }
 });
+
+var sendNotificationByPayload = function(payload) {
+    sendNotification(payload.type, payload.title, payload.message, payload.timeOffset);
+}
+
+var sendNotification = function(type, title, message, timeOffset) {
+    console.log('Sending notification with 5s delay')
+    chrome.notifications.create('reminder', {
+        type: 'basic',
+        iconUrl: 'graphics/icons/icon.png',
+        title: 'Don\'t forget!',
+        message: 'Wake up, dude!',
+        eventTime: Date.now() + 5000
+     }, function(notificationId) { console.log });
+}
--- scripts/helper.js
+++ scripts/helper.js
@@ -5,7 +5,17 @@
 	Utpressing: 960 sec
 */
 /*
-			var premCounters = [['premCounter_airport',null],['premCounter_kriminalitet',null],['premCounter_gta',null],['premCounter_blackmail',null],['premCounter_oc',null],['premCounter_cdg',null],['premCounter_jail',null],['premCounter_fightclub',null],['premCounter_filmmaking',null],['premCounter_hire_bodyguard',null],['premCounter_weapontraining',null]];
+            var premCounters = [['premCounter_airport',null],
+            ['premCounter_kriminalitet',null],
+            ['premCounter_gta',null],
+            ['premCounter_blackmail',null],
+            ['premCounter_oc',null],
+            ['premCounter_cdg',null],
+            ['premCounter_jail',null],
+            ['premCounter_fightclub',null],
+            ['premCounter_filmmaking',null],
+            ['premCounter_hire_bodyguard',null],
+            ['premCounter_weapontraining',null]];
 			var premCounter_loaded = false;
 
 			var premCounters_oc = 0;
@@ -20,12 +30,43 @@
 
 var foundKrimCounter = !!document.getElementById('premCounter_kriminalitet');
 
-if (foundKrimCounter) {
-    alert('Success');
-} else {
-    alert('Error');
+if (!foundKrimCounter) {
+    console.log('Error');
 }
 
-chrome.runtime.sendMessage({text: "hey"}, function(response) {
-    console.log("Response: ", response);
+$('#krimSubmitButton').click(function() {
+    sendNotification(
+    'krim', 
+    'Kriminalitet',
+    'Kriminalitet nu muligt igen',
+    180);
 });
+$('#gtaSubmitButton').click(function() {
+    sendNotification(
+    'gta', 
+    'Biltyveri',
+    'Biltyveri nu muligt igen',
+    360);
+})
+$('input[name=submitBlackmail]').click(function() {
+    sendNotification(
+    'blackmail', 
+    'Utpresning',
+    'Utpresning nu muligt igen',
+    960);
+})
+
+var sendNotification = function (type, title, message, timeOffset) {
+    console.log('ContentScript sending notification for', type)
+    chrome.runtime.sendMessage({
+        action: 'sendNotification',
+        payload: {
+            type: type,
+            title: title,
+            message: message,
+            timeOffset: timeOffset
+        }
+    }, function(response) {
+        console.log("Response: ", response);
+    });
+}
