60f322726a405966c1cea8210271cbf4f72f38ed
Stefan Bols <stefan@imbols.net>
2018-11-08 16:50:52 +0100

    Added page timers boilerplate

---
+++ pages/timers.js
@@ -0,0 +1,61 @@
+$(function() {
+    var $timerContainer = $('#timers');
+    var $timers = $('<ul>');
+
+    /// One timer
+    var $timer = $('<li>');
+    var when = 1541098747000; // November 1, 2018 6:59:07 PM
+    var now = new Date().getTime();
+    var diff = when - now;
+    $timer.text(diff);
+    $timer.appendTo($timers);
+
+    $timers.appendTo($timerContainer);
+
+    
+    var deadline = new Date(when);
+    initializeClock('clockdiv', deadline);
+});
+
+var drawTimers = () => {
+
+}
+
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+  
+  function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+  
+    function updateClock() {
+      var t = getTimeRemaining(endtime);
+  
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+  
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+  }
